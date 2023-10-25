6011cc2168a105c13e4dff9358191d7e1a56f099
Kasper Laudrup <laudrup@stacktrace.dk>
2023-10-25 20:54:12 +0200

    Add LiCENSE file
    Instead of an annoying header in all header files add the GPL v3 as
    the license used for this project.

--- src/Main.h
+++ src/Main.h
@@ -15,27 +15,6 @@
 #include <string>
 #include <vector>
 
-/*
-  Main.java
-
-  Arkanerd - An Arkanoid/Breakout like game for J2ME mobile phones
-  Copyright (C) 2006  Kasper Laudrup
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 namespace arkanerd {
 
 class Main : public j2me::MIDlet, j2me::CommandListener {
