e622148926e88d494f47c43843d4d19de4908200
Kasper Laudrup <laudrup@stacktrace.dk>
2023-10-28 18:32:19 +0200

    Cleanup TitleCanvas class

--- src/TitleCanvas.cpp
+++ src/TitleCanvas.cpp
@@ -6,29 +6,26 @@ namespace arkanerd {
 TitleCanvas::TitleCanvas(Main* main)
   : j2me::Canvas(main)
   , main_(main) {
-  setFullScreenMode(true);
-  width_ = getWidth();
-  height_ = getHeight();
   f_ = j2me::Font::getFont(j2me::Font::FACE_SYSTEM, j2me::Font::STYLE_PLAIN, j2me::Font::SIZE_SMALL);
 
   text_ = std::make_unique<TextLayer>("arkanerd", 8);
-  bg_image_ = j2me::Image::createImage("/images/titlebg.png");
+  auto bg_image = j2me::Image::createImage("/images/titlebg.png");
 
-  text_->setPosition((width_ - text_->getWidth()) / 2, (height_ - text_->getHeight()) / 2);
-  int rows = (height_ / bg_image_.getHeight()) + 1;
-  bg_layer_ = std::make_unique<j2me::TiledLayer>(1, rows, bg_image_, bg_image_.getWidth(), bg_image_.getHeight());
-  bg_layer_->fillCells(0,0,1,rows,1);
+  text_->setPosition((getWidth() - text_->getWidth()) / 2, (getHeight() - text_->getHeight()) / 2);
+  int rows = (getHeight() / bg_image.getHeight()) + 1;
+  bg_layer_ = std::make_unique<j2me::TiledLayer>(1, rows, bg_image, bg_image.getWidth(), bg_image.getHeight());
+  bg_layer_->fillCells(0, 0, 1, rows, 1);
 }
 
 void TitleCanvas::paint(j2me::Graphics *g) {
   bg_layer_->paint(g);
   text_->paint(g);
   g->setColor(0x00000000);
-  g->drawString(S1_, width_ / 2, height_ / 2 + height_ / 4, j2me::Graphics::TOP | j2me::Graphics::HCENTER);
-  g->drawString(S2_, width_ / 2, height_ / 2 + height_ / 4 + f_.getHeight(), j2me::Graphics::TOP | j2me::Graphics::HCENTER);
+  g->drawString(S1_, getWidth() / 2, getHeight() / 2 + getHeight() / 4, j2me::Graphics::TOP | j2me::Graphics::HCENTER);
+  g->drawString(S2_, getWidth() / 2, getHeight() / 2 + getHeight() / 4 + f_.getHeight(), j2me::Graphics::TOP | j2me::Graphics::HCENTER);
   g->setColor(0x00DD2222);
-  g->drawString(S1_, width_ / 2, (height_ / 2 + height_ / 4) + 2, j2me::Graphics::TOP | j2me::Graphics::HCENTER);
-  g->drawString(S2_, width_ / 2, (height_ / 2 + height_ / 4 + f_.getHeight()) + 2, j2me::Graphics::TOP | j2me::Graphics::HCENTER);
+  g->drawString(S1_, getWidth() / 2, (getHeight() / 2 + getHeight() / 4) + 2, j2me::Graphics::TOP | j2me::Graphics::HCENTER);
+  g->drawString(S2_, getWidth() / 2, (getHeight() / 2 + getHeight() / 4 + f_.getHeight()) + 2, j2me::Graphics::TOP | j2me::Graphics::HCENTER);
 }
 
 void TitleCanvas::keyPressed(int /*key*/) {
--- src/TitleCanvas.h
+++ src/TitleCanvas.h
@@ -4,11 +4,9 @@
 
 #include "j2me/Canvas.h"
 #include "j2me/Font.h"
-#include "j2me/Image.h"
 #include "j2me/Graphics.h"
 
 #include <string>
-#include <any>
 
 namespace arkanerd {
 class Main;
@@ -17,20 +15,17 @@ class TitleCanvas : public j2me::Canvas {
 
 public:
   TitleCanvas(Main* main);
-  void keyPressed(int key) override;
+  void keyPressed(int key) final;
 
 protected:
-  void paint(j2me::Graphics* g) override;
+  void paint(j2me::Graphics* g) final;
 
 private:
-  int width_ = 0;
-  int height_ = 0;
   Main* main_;
   std::unique_ptr<TextLayer> text_;
   j2me::Font f_;
   const std::string S1_ = "Kasper Laudrup";
   const std::string S2_ = "Copyright (c) 2006/7";
-  j2me::Image bg_image_;
   std::unique_ptr<j2me::TiledLayer> bg_layer_;
 };
 
